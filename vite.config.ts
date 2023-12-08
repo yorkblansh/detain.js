71c090003471d38c68bb62a1faf73aa3d1384e54
Oleg Chepelev <yorkblansh@gmail.com>
2023-12-08 18:07:22 +0300

    remove unused imports

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import dts from 'vite-plugin-dts'
-import { EsLinter, linterPlugin } from 'vite-plugin-linter'
 
 // https://vitejs.dev/config/
 export default defineConfig((configEnv) => ({
